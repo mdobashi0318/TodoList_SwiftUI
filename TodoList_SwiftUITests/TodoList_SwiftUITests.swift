0f8fdde4ffec43aab05ef64ab3f664acda2cffdc
Masaharu Dobashi <dobashi.masaharu@gmail.com>
2020-05-16 21:59:00 +0900

    Initial Commit

---
+++ TodoList_SwiftUITests/TodoList_SwiftUITests.swift
@@ -0,0 +1,34 @@
+//
+//  TodoList_SwiftUITests.swift
+//  TodoList_SwiftUITests
+//
+//  Created by 土橋正晴 on 2020/05/16.
+//  Copyright © 2020 m.dobashi. All rights reserved.
+//
+
+import XCTest
+@testable import TodoList_SwiftUI
+
+class TodoList_SwiftUITests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
