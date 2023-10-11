3869da248bd338e760ad6b8a7f166a71d9894747
MasaharuDobashi <masaharu.dobashi@gmail.com>
2023-10-12 00:14:43 +0900

    ヘッダーのボタンに選択状態のバーを追加

--- TodoList_SwiftUI/Views/ListHeader.swift
+++ TodoList_SwiftUI/Views/ListHeader.swift
@@ -11,25 +11,52 @@ struct ListHeader: View {
     
     @Binding var segmentIndex: SegmentIndex
     
+    @State var positionX : CGFloat = -UIScreen.main.bounds.width /  2.6
+    
     var body: some View {
         ScrollView(.horizontal) {
-            HStack(spacing: 1) {
-                ForEach(0..<SegmentIndex.allCases.count, id: \.self) { index in
-                    Button(action: {
-                        guard let selectIndex = SegmentIndex(rawValue: index) else { return }
-                        segmentIndex = selectIndex
-                    }, label: {
-                        Text(buttonText(index))
-                    })
-                    .frame(minWidth: UIScreen.main.bounds.width / CGFloat(SegmentIndex.allCases.count) , minHeight: 40)
-                    // TODO: 色を変更
-                    .background(segmentIndex == SegmentIndex(rawValue: index) ? Color(uiColor: UIColor.systemBackground) : Color(uiColor: UIColor.systemGray))
+            ZStack(alignment: .bottom) {
+                HStack(spacing: 1) {
+                    ForEach(0..<SegmentIndex.allCases.count, id: \.self) { index in
+                        Button(action: {
+                            guard let selectIndex = SegmentIndex(rawValue: index) else { return }
+                            segmentIndex = selectIndex
+                        }, label: {
+                            Text(buttonText(index))
+                        })
+                        .frame(minWidth: UIScreen.main.bounds.width / CGFloat(SegmentIndex.allCases.count) , minHeight: 40)
+                    }
                 }
+                Rectangle()
+                    .frame(width: UIScreen.main.bounds.width / CGFloat(SegmentIndex.allCases.count) / 1.5, height: 5)
+                    .foregroundColor(.red)
+                    .offset(x: positionX, y: 0)
+                
             }
         }
+        .task(id: segmentIndex) {
+            selectedBar(segmentIndex.rawValue)
+        }
+        
     }
     
-    
+    private func selectedBar(_ index: Int) {
+        withAnimation(.default) {
+            // TODO: バーの位置を調整
+            switch index {
+            case 0:
+                positionX = -UIScreen.main.bounds.width /  2.6
+            case 1:
+                positionX = -UIScreen.main.bounds.width /  8.1
+            case 2:
+                positionX = UIScreen.main.bounds.width /  8.1
+            case 3:
+                positionX = UIScreen.main.bounds.width /  2.7
+            default:
+                break
+            }
+          }
+    }
     
     private func buttonText(_ index: Int) -> String {
         switch index {
